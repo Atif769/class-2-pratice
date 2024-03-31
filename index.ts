a2c8712fa8f8629f3c3212842113eb7b2f7b3da5
Muhammad Atif <m.atif769@gmail.com>
2024-03-31 04:14:25 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("hello");
+// values can be change at any time variable declare with let
+let firstName;
+firstName = "Atif";
+// values can not be change at any time variable declare with const
+const batch = "batch 57";
